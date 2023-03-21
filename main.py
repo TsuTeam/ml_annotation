e130abf11010d2fb3b446146b5b43ecdc2cc0a61
Dmitry <assist@inbox.ru>
2023-03-21 05:47:59 +0500

    Last ver

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import streamlit as st
 
 st.title('Web-приложение для создания аннотаций к тексту')
-st.markdown('Приложение использует предварительно обученную модель "IlyaGusev/mbart_ru_sum_gazeta"(mBART), полученную '
+st.markdown('Приложение использует предварительно обученную модель "IlyaGusev/mbart_ru_sum_gazeta", полученную '
             'с Hugging Face (https://huggingface.co/) и используемую для обобщения текста на русском языке.')
 
 text = st.text_area('Исходный текст:')
