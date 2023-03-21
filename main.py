7922bdc36d947bad6d730003f78f3a1b5e8369ef
Dmitry <assist@inbox.ru>
2023-03-21 06:28:13 +0500

    Change model name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import streamlit as st
 
 st.title('Web-приложение для создания аннотаций к тексту')
-st.markdown('Приложение использует предварительно обученную модель "IlyaGusev/mbart_ru_sum_gazeta", полученную '
+st.markdown('Приложение использует предварительно обученную модель "IlyaGusev/rut5_base_sum_gazeta", полученную '
             'с Hugging Face (https://huggingface.co/) и используемую для обобщения текста на русском языке.')
 
 text = st.text_area('Исходный текст:')
